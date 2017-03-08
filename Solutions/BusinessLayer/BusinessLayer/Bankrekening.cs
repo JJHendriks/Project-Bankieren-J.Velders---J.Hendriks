7015eecfdfb49b49fea164d94a16c850613a6516
Jim Hendriks <jjhendriks@outlook.com>
2017-03-08 11:42:51 +0100

    Alle gekkigheid bij elkaar

---
+++ Solutions/BusinessLayer/BusinessLayer/Bankrekening.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLayer
+{
+   public class Bankrekening : Rekening
+    {
+        private decimal maximaalKrediet;
+
+        public Bankrekening( string _rekeningnr, decimal _banksaldo, decimal _maximaalkrediet) : base(_rekeningnr, _banksaldo)
+        {
+            this.maximaalKrediet = _maximaalkrediet;
+        }
+
+        public bool OverboekenSpaarRekening(decimal bedrag)
+        {
+            
+            return true;
+        }
+
+        public bool BetalingVerrichten(string rekeningnr, decimal bedrag)
+        {
+            return true;
+        }
+    }
+}
---
+++ Solutions/BusinessLayer/BusinessLayer/SpaarRekening.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLayer
+{
+    public class SpaarRekening : Rekening
+    {
+        private decimal Rentepercentage;
+
+        public decimal HuidigeRenteBerekenen
+        {
+            get { return (Banksaldo * Convert.ToDecimal(0.01)); }
+        }
+
+        public SpaarRekening(string _rekeningnr, decimal _banksaldo, decimal _rente) : base(_rekeningnr, _banksaldo)
+        {
+            this.Rentepercentage = _rente;
+        }
+
+        public override string ToString()
+        {
+            return base.ToString() + 
+                string.Format("RentePercentage: {0}\nHuidig Opgebouwede Rente: {1}", Rentepercentage, HuidigeRenteBerekenen);
+        }
+
+        public bool OverboekenNaarBetaalrekening(decimal bedrag)
+        {
+            //Moet nog verder aangevuld worden
+            return true;
+        }
+
+    }
+}
