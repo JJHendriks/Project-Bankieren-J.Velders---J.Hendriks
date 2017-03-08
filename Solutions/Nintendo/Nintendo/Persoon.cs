b8dcc5a8824055ed22f779cadbaca578593e0aee
jevolo <jvel1998@gmail.com>
2017-03-08 13:25:46 +0100

    MyProject01

---
+++ Solutions/Nintendo/Nintendo/Persoon.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Nintendo
+{
+    public class Persoon
+    {
+        private string voornaam;
+
+        private string achternaam;
+
+        private long bsn;
+
+        private string gebruikersnaam;
+
+        private string wachtwoord;
+
+        public Persoon(string _voornaam,string _achternaam,long _bsn)
+        {
+            this.voornaam = _voornaam;
+            this.achternaam = _achternaam;
+            this.bsn = _bsn;
+        }
+
+        public override string ToString()
+        {
+            return string.Format("Voornaam: {0}\nAchternaam: {1}\nbsn: {2}\ngebruikersnaam: {3}\nwachtwoord: {4}\n",this.voornaam,this.achternaam,this.bsn,this.gebruikersnaam,this.wachtwoord);
+        }
+    }
+}
---
+++ Solutions/Nintendo/Nintendo/Rekening.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLayer
+{
+    public abstract class Rekening
+    {
+        /// <summary>
+        /// Protected fields RekeningNr en Banksaldo
+        /// </summary>
+        protected string RekeningNr;
+
+        protected decimal Banksaldo;
+
+        /// <summary>
+        /// Properties voor Geld BijSchrijven op het Banksaldo en om Geld af te schrijven van het Banksaldo
+        /// </summary>
+        public decimal BijSchrijven
+        {
+            set { value += Banksaldo; }
+        }
+
+        public decimal AfSchrijven
+        {
+            set { value -= Banksaldo; }
+        }
+
+
+        public Rekening(string _rekeningnr, decimal _banksaldo)
+        {
+            this.RekeningNr = _rekeningnr;
+            this.Banksaldo = _banksaldo;
+        }
+
+        public override string ToString()
+        {
+            return string.Format("RekeningNr: {0} \nBanksaldo: {1}", this.RekeningNr, this.Banksaldo);
+        }
+
+
+
+    }
+}
