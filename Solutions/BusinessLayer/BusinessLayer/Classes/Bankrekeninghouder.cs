41d496f495a370bb8d2d374ce9f7898237d93576
jevolo <jvel1998@gmail.com>
2017-03-14 08:40:27 +0100

    biem

--- Solutions/BusinessLayer/BusinessLayer/Classes/Bankrekeninghouder.cs
+++ Solutions/BusinessLayer/BusinessLayer/Classes/Bankrekeninghouder.cs
@@ -8,19 +8,35 @@ namespace BusinessLayer
 {
     public class Bankrekeninghouder
     {
+        /// <summary>
+        /// Een simpele class die naam, bsn en inlogdata bij houd.
+        /// </summary>
         private Persoon rekeninghouder;
 
+        /// <summary>
+        /// Een rekening die bedoeld is om te betalen.
+        /// </summary>
         private BetaalRekening betaalrekening;
 
+        /// <summary>
+        /// Een rekening die bedoeld is om te sparen.
+        /// </summary>
         private SpaarRekening spaarrekening;
 
-        //public Bankrekeninghouder( Persoon _rekeninghouder, Bankrekening _bankrekening, SpaarRekening _spaarrekening)
-        //{
-        //    this.rekeninghouder = _rekeninghouder;
-        //    this.bankrekening = _bankrekening;
-        //    this.spaarrekening = _spaarrekening;
-        //}
-
+        /// <summary>
+        /// De constuctor van de bankrekeinghouder.
+        /// </summary>
+        /// <param name="voornaam">De voornaam van de Bankrekeninghouder.</param>
+        /// <param name="achternaam">De achternaam van de bankrekeninghouder.</param>
+        /// <param name="bsn">Het burger service number.</param>
+        /// <param name="gebruikersnaam">De gebruikersnaam van de bankrekeninghouder.</param>
+        /// <param name="wachtwoord">Het wachtwoord van de bankrekinngohouder.</param>
+        /// <param name="rekeningnrSparen">De IBAN van de spaarrekening.</param>
+        /// <param name="spaarSaldo">Het saldo van de spaarrekening.</param>
+        /// <param name="rentepercentage">De rente.</param>
+        /// <param name="rekeningnrBetalen">De IBAN van de betaalrekening.</param>
+        /// <param name="BetaalSaldo">Het saldo van de betaalrekening.</param>
+        /// <param name="maxkrediet">Het maximaal bedrag dat u in de min mag staan.</param>
         public Bankrekeninghouder(string voornaam, string achternaam, long bsn, string gebruikersnaam, string wachtwoord,
             string rekeningnrSparen, decimal spaarSaldo, decimal rentepercentage ,string rekeningnrBetalen, decimal BetaalSaldo, decimal maxkrediet)
         {
@@ -29,19 +45,37 @@ namespace BusinessLayer
             betaalrekening = new BetaalRekening(rekeningnrBetalen, BetaalSaldo, maxkrediet);
         }
 
+        /// <summary>
+        /// Deze methode retouneert de Gebruikernaam.
+        /// </summary>
+        /// <returns>De Gebuikersnaam</returns>
         public string Gebruikersnaam()
         {
             return rekeninghouder.Gebruikersnaam;
         }
+
+        /// <summary>
+        /// Deze methode retouneert Het wachtwoord.
+        /// </summary>
+        /// <returns>Het wachtwoord</returns>
         public string Wachtwoord()
         {
             return rekeninghouder.Wachtwoord;
         }
+
+        /// <summary>
+        /// Deze methode retouneert De IBAN van de spaarrekening.
+        /// </summary>
+        /// <returns>De spaarrekening als een string</returns>
         public string SpaarRekeningInzien()
         {
          return spaarrekening.ToString();
         }
 
+        /// <summary>
+        /// Deze methode retouneert De IBAN van de betaalrekening.
+        /// </summary>
+        /// <returns>De betaalrekening als een string</returns>
         public string BankRekeningInzien()
         {
             return betaalrekening.ToString();
@@ -53,12 +87,17 @@ namespace BusinessLayer
             return true;
         }
 
+        /// <summary>
+        /// Schrijft het bedrag van Betaalrekening naar de spaarrekening.
+        /// </summary>
+        /// <param name="bedrag">De hoeveelheid wat er overgeschreven word.</param>
+        /// <returns>een true als het gelukt is een false als het mislukt.</returns>
         public bool OverboekenNaarSpaarRekening(decimal bedrag)
         {
             try
             {
                 betaalrekening.AfSchrijven(bedrag);
-                spaarrekening.BijSchrijven(bedrag);
+                spaarrekening.Bijschrijven = bedrag;
                 return true;
             }
 
@@ -69,6 +108,11 @@ namespace BusinessLayer
             }
         }
 
+        /// <summary>
+        /// Schrijft het bedrag van spaarrekening naar de Betaalrekening.
+        /// </summary>
+        /// <param name="bedrag">De hoeveelheid wat er overgeschreven word.</param>
+        /// <returns>een true als het gelukt is een false als het mislukt.</returns>
         public bool OverboekenNaarBetaalRekening(decimal bedrag)
         {
             try
