fe550941a591896c79c20fc42ad76f2b098f0694
Jim Hendriks <jjhendriks@outlook.com>
2017-03-15 14:02:02 +0100

    jrojfo

--- Solutions/BusinessLayer/BusinessLayer/Bank.cs
+++ Solutions/BusinessLayer/BusinessLayer/Bank.cs
@@ -21,13 +21,28 @@ namespace BusinessLayer
 
         }
 
-        public void ToevoegenBankrekeninghouder()
+        public Bank(string _bankNaam, string _gebruikersnaam, string _wachtwoord)
         {
+            this.bankNaam = _bankNaam;
+            this.gebruikersnaam = _gebruikersnaam;
+            this.wachtwoord = _wachtwoord;
+        }
 
+        public void ToevoegenBankrekeninghouder(string _voornaam, string _achternaam, long _bsn, string _gebruikersnaam, string _wachtwoord,
+            string _rekeningnrSparen, decimal _spaarSaldo, decimal _rentepercentage, string _rekeningnrBetalen, decimal _BetaalSaldo, decimal _maxkrediet)
+        {
+            Bankrekeninghouder Bankrekeninghouder = new Bankrekeninghouder(_voornaam, _achternaam, _bsn, _gebruikersnaam, _wachtwoord, _rekeningnrSparen, _spaarSaldo, _rentepercentage, _rekeningnrBetalen, _BetaalSaldo, _maxkrediet);
+
+            //DataProvider.AlleBankrekeningHouders
         }
 
         public bool BankRekeninghouderOpheffen(long _bsn)
         {
+            /*
+            AlleBankrekeningHouders.remove(..);
+
+
+            */
             return true;
         }
 
